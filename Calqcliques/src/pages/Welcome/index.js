6d14125dde75d98caf11b951b22cc02bdcf053a7
wendelcliques <70993835+wendelcliques@users.noreply.github.com>
2021-04-09 00:36:56 -0300

    atual

--- Calqcliques/src/pages/Welcome/index.js
+++ Calqcliques/src/pages/Welcome/index.js
@@ -1,12 +1,55 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 
-const Welcome = () => {
+import Colors from '../../styles/Colors';
+
+const Welcome = ({navigation}) => {
     return (
-        <View>
-            <Text>Welcome</Text>
+        <View style={styles.container}>
+
+            <TouchableOpacity 
+            onPress={() => navigation.navigate('Treinamento')}
+            style={styles.button}>
+                <Text 
+                style={[styles.paragraph,
+                    {color: Colors.white,
+                        fontWeight: 'bold',
+                        fontSize: 16,}
+                
+                ]} 
+                
+                >Treinamento</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+            onPress={() => navigation.navigate('Aventura')}
+            style={styles.button}>
+                <Text style={styles.buttonText}>Aventura</Text>
+            </TouchableOpacity>
+            
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.champagne,
+    },
+    button: {
+        height: 20,
+        width: '40%',
+        backgroundColor: Colors.carbon,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    buttonText: {
+        color: Colors.white,
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
+});
 
 export default Welcome
