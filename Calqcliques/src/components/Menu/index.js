d291f41637afda55eec6de8e2da76e10e231ce06
wendelcliques <70993835+wendelcliques@users.noreply.github.com>
2021-05-12 01:26:59 -0300

    atual

--- Calqcliques/src/components/Menu/index.js
+++ Calqcliques/src/components/Menu/index.js
@@ -3,30 +3,53 @@ import Svg, {Ellipse, Polygon} from 'react-native-svg'
 import { View, Text, StyleSheet } from 'react-native'
 
 import Container from '../Container'
+import EntryList from '../EntryList'
 
 const Menu = () => {
     return (
         <Container
         title="Menu"
         >
-        <View>
-            <Svg height="100" width="100" >
+        <View style={{backgroundcolor: "red", 
+        flexDirection: 'row'}}>
+            <Svg height="80" width="100" >
                 <Polygon
-                points="10,10 15,50 95,50"
-                fill="lime"
+                points="54,0 15,62 95,62"
+                stroke="purple"
+                strokeWidth="2"
+                fill="rgba(99, 99, 00, 1.0)"
                 >
 
                 </Polygon>
                 <Ellipse
                     cx="55"
-                    cy="55"
+                    cy="65"
                     rx="40"
-                    ry="5"
+                    ry="10"
                     stroke="purple"
                     strokeWidth="2"
-                    fill="yellow"
+                    fill="rgba(79, 79, 00, 1.0)"
+                />
+
+<Polygon
+                points="54,7 24,62 85,62"
+                stroke="purple"
+                strokeWidth="2"
+                fill="rgba(00, 00, 99, 0.2)"
+                >
+
+                </Polygon>
+                <Ellipse
+                    cx="55"
+                    cy="65"
+                    rx="30"
+                    ry="7"
+                    stroke="purple"
+                    strokeWidth="2"
+                    fill="rgba(00, 00, 50, 01.0)"
                 />
             </Svg>
+            <EntryList />
             </View>
         </Container>
     )
